a9d08faa8be26a70e09adf5118acde415891e7b1
Velociraptor45 <30436688+Velociraptor45@users.noreply.github.com>
2023-01-21 20:34:07 +0100

    #17: Fix loading already loaded Assembly not correctly

--- TestCodeGenerator/Generator/Files/FileHandler.cs
+++ TestCodeGenerator/Generator/Files/FileHandler.cs
@@ -6,7 +6,7 @@ public class FileHandler : IFileHandler
 {
     public Assembly LoadAssembly(string dllPath)
     {
-        return Assembly.LoadFrom(dllPath);
+        return Assembly.LoadFile(dllPath);
     }
 
     public bool FileExits(string filePath)
