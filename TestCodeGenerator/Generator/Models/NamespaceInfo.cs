790c8730bab599d1fdc5a0c6cb4ef6facc3855e2
Velociraptor45 <30436688+Velociraptor45@users.noreply.github.com>
2023-08-06 09:12:37 +0200

    #14: Implement matching output folder to builder namespace

---
+++ TestCodeGenerator/Generator/Models/NamespaceInfo.cs
@@ -0,0 +1,41 @@
+﻿namespace TestCodeGenerator.Generator.Models;
+public class NamespaceInfo
+{
+    public NamespaceInfo(Type type, string outputRootNamespace)
+    {
+        var rootNamespaceParts = outputRootNamespace.Split('.');
+        var typeNamespaceParts = type.Namespace!.Split('.');
+
+        var maxLength = Math.Max(rootNamespaceParts.Length, typeNamespaceParts.Length);
+
+        for (int i = 0; i < maxLength; i++)
+        {
+            if (typeNamespaceParts.Length <= i)
+            {
+                BuilderClassNamespace = outputRootNamespace;
+                InAssemblyPath = string.Empty;
+                return;
+            }
+
+            if (rootNamespaceParts.Length <= i)
+            {
+                BuilderClassNamespace = type.Namespace!;
+                InAssemblyPath = string.Join('.', typeNamespaceParts[i..]);
+                return;
+            }
+
+            if (rootNamespaceParts[i] == typeNamespaceParts[i])
+                continue;
+
+            InAssemblyPath = string.Join('.', typeNamespaceParts[i..]);
+            BuilderClassNamespace = $"{string.Join('.', rootNamespaceParts)}.{InAssemblyPath}";
+            return;
+        }
+
+        BuilderClassNamespace = outputRootNamespace;
+        InAssemblyPath = string.Empty;
+    }
+
+    public string BuilderClassNamespace { get; }
+    public string InAssemblyPath { get; }
+}
